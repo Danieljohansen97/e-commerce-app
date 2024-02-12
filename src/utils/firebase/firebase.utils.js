d97b72ff09a3dd159971292786ed674b287650f9
Danieljohansen97 <danieljohansen1997@hotmail.com>
2024-02-12 23:30:55 +0100

    Remove TODO in firebase.utils.js

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -8,7 +8,6 @@ import {
     getDoc,
     setDoc
 } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
